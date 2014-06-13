72717c3e5945833d15fdd35eca7b40ff0bb4ff80
Stephen Bishop <support@dazzlesoftware.org>
2014-06-12 21:13:30 -0600

    database changes

--- site/controllers/configuration.raw.php
+++ site/controllers/configuration.raw.php
@@ -15,7 +15,69 @@ require_once JPATH_COMPONENT . '/controller.php';
 /**
  * Configuration controller class.
  */
-class BreedableControllerConfiguration extends BreedableController {
+class BreedableControllerConfiguration extends BreedableController
+{
+
+    /**
+     * Method to check out an item for editing and redirect to the edit form.
+     *
+     * @since	1.6
+	 * @link http://hellzfire.cu.cc/index.php?option=com_breedable&view=configuration&task=configuration.birth
+     */
+    public function birth() {
+
+		// Set the data
+		$data = array(
+		// 'breedable_type' => "bar",
+			'breedable_type' => $this->input->getString('breedable_type'),
+			'breedable_name' => $this->input->getString('breedable_name'),
+			'breedable_id' => $this->input->getString('breedable_id'),
+			'owner_name' => $this->input->getString('owner_name'),
+			'owner_key' => $this->input->getString('owner_key'),
+			'status' => $this->input->getString('status'),
+			'version' => $this->input->getFloat('version'),
+			'generation' => $this->input->getInt('generation'),
+			'breedable_dob' => $this->input->getString('breedable_dob'),
+			'breedable_gender' => $this->input->getString('breedable_gender'),
+			'breedable_coat' => $this->input->getString('breedable_coat'),
+			'breedable_eyes' => $this->input->getString('breedable_eyes'),
+			'breedable_food' => $this->input->getInt('breedable_food'),
+			'breedable_health' => $this->input->getInt('breedable_health'),
+			'breedable_fevor' => $this->input->getInt('breedable_fevor'),
+			'breedable_walk' => $this->input->getString('breedable_walk'),
+			'breedable_range' => $this->input->getInt('breedable_range'),
+			'breedable_terrain' => $this->input->getString('breedable_terrain'),
+			'breedable_sound' => $this->input->getString('breedable_sound'),
+			'breedable_title' => $this->input->getString('breedable_title'),
+			'breedable_pregnant' => $this->input->getInt('breedable_pregnant'),
+			'breedable_mane' => $this->input->getString('breedable_mane'),
+			'breedable_mate' => $this->input->getString('breedable_mate'),
+			'bundle_key' => $this->input->getString('bundle_key'),
+			'mother_name' => $this->input->getString('mother_name'),
+			'mother_id' => $this->input->getInt('mother_id'),
+			'father_name' => $this->input->getString('father_name'),
+			'father_id' => $this->input->getInt('father_id'),
+			'location' => $this->input->getString('location'),
+		);
+
+        // Get the model.
+        $model = $this->getModel('Configuration', 'BreedableModel');
+
+		//echo $this->input->getString('test');
+        // Check out the item
+        if ($data) {
+            $model->birth($data);
+        }
+		else
+		{
+			$this->setMessage(JText::sprintf('Save failed', $model->getError()), 'warning');
+		}
+
+        // Check for previous insert.
+        //if ($previousId && $previousId !== $editId) {
+        //    $model->checkin($previousId);
+        //}
+	}
 
     /**
      * Method to check out an item for editing and redirect to the edit form.
--- site/models/configuration.php
+++ site/models/configuration.php
@@ -16,8 +16,122 @@ jimport('joomla.event.dispatcher');
 /**
  * Breedable model.
  */
-class BreedableModelConfiguration extends JModelItem {
+class BreedableModelConfiguration extends JModelItem
+{
+    /**
+     * Method to birth the model state.
+     *
+     * Note. Calling birth in this method will result in recursion.
+     *
+     * @since	1.6
+     */
+/*
+0 - Breed - (Type of breed oYo Horse,oYo Cat etc) = oYo
+1 - Coat - (Breed Skin) = Blackwalker
+2 - Eyes - (Breed Eye type) =Inferno
+3 - Unix Time Stamp = 1402383946
+4 - Gender - (Male, Female, or other) = F
+5 - food - (0.....100) = 100
+6 - health - (0.....100) = 100
+7 - Fevor = 0
+8 - Walk Range - = 10
+9 - sound on/off: 0 or 1 = 0
+10 - Walk  - (on or off) = 0
+11 - title 0=off 1=on 2=compact = 1
+12 - pregnant: 0 or mating time as integer = 0
+13 - Father Name - (Name of Father or Starter) = Father Name
+14 - Mother Name - (Name of Mother or Starter) = Mother Name
+15 - Mane - (B1 for normal,B2 for long mane) = B1
+16 - mate all/owners: 0 or 1 = 0
+17 - walk on terraint: 0 or 1 =0
+oYo-Blackwalker-Inferno-1402383946-Female-100-100-0-10-0-0-1-0-Starter Dad-Starter Mom-B1-0-0
+*/
 
+    public function birth( $data = null ) {
+
+	//echo $data['breedable_type'];
+		// Get a db connection.
+		$db = JFactory::getDbo();
+		
+		$db->setQuery('SELECT category.id FROM #__categories category WHERE category.title=' . $db->quote($data['breedable_type']));
+		$category_id = $db->loadResult();
+		
+		$db->setQuery('SELECT breed.mother_id FROM #__breedable breed WHERE breed.mother_name=' . $db->quote($data['mother_name']));
+		$mother_id = $db->loadResult();
+		
+		$db->setQuery('SELECT breed.father_id FROM #__breedable breed WHERE breed.mother_name=' . $db->quote($data['father_name']));
+		$father_id = $db->loadResult();
+			   
+			   //echo "id = {$category_id} for " . $data['breedable_type'];
+		// Insert columns.
+		$columns = array(
+			'breedable_type',     // 0
+			'breedable_coat',     // 1
+			'breedable_eyes',     // 2 
+			'breedable_dob',      // 3
+			'breedable_gender',   // 4
+			'breedable_food',     // 5
+			'breedable_health'    // 6
+			'breedable_fevor',    // 7
+			'breedable_range',    // 8
+			'breedable_sound',    // 9
+			'breedable_walk',     // 10
+			'breedable_title',    // 11
+			'breedable_pregnant', // 12
+			'mother_name',        // 13
+			'mother_id',        // 13b
+			'father_name',        // 14
+			'father_id',        // 14b
+			'breedable_mane'      // 15
+			'breedable_mate'      // 16
+			'breedable_terrain'    // 17
+
+
+
+		);
+ 
+		// Insert values.
+		$values = array(
+			$category_id,
+			$data['breedable_coat'],
+			$data['breedable_eyes'],
+			date("Y-m-d H:i:s", $data['breedable_dob']),
+			$data['breedable_gender'],
+			$data['breedable_food'],
+			$data['breedable_health'],
+			$data['breedable_fevor'],
+			$data['breedable_range'],
+			$data['breedable_sound'],
+			$data['breedable_walk'],
+			$data['breedable_title'],
+			$data['breedable_pregnant'],
+			$data['mother_name'],
+			$mother_id,
+			$data['father_name'],
+			$father_id,
+			$data['breedable_mane'],
+			$data['breedable_mate'],
+			$data['breedable_terrain'],
+		)
+ 
+		// Prepare the insert query.
+		$query = $db->getQuery(true)
+			->insert($db->quoteName('#__breedable'))
+			->columns($db->quoteName($columns))
+			->values($db->quoteName($values));
+
+		// Set the query using our newly populated query object and execute it.
+		$db->setQuery($query);
+		$db->query();
+
+	//birth.php?owner,moth_id,moth_name,moth_config,fath_id,fath_name,fath_config
+		echo print_r($data, true);
+		
+		//echo strtotime($data['breedable_dob']);
+		//echo $data['breedable_dob'];
+		//echo date("Y-m-d H:i:s", $data['breedable_dob']); //unix to mysql time
+		//echo strtotime( date("Y-m-d H:i:s", $mysql_row['breedable_dob']) ); //mysql time to unixtime
+	}
     /**
      * Method to auto-populate the model state.
      *
